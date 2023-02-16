2905c43abeff8fb1c22759acc03e70ac0c450991
nikolaitch <nikolaitch71@gmail.com>
2023-02-16 22:16:14 +0300

    Тест

---
+++ main/java/lesson1/Response.java
@@ -0,0 +1,23 @@
+package lesson1;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Data
+public class Response {
+    @JsonProperty("cuisine")
+    private String cuisine;
+    @JsonProperty("cuisines")
+    private List<String> cuisines = null;
+    @JsonProperty("confidence")
+    private Double confidence;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+}
